91c655b066b79dbf802b053d5f3eb9be58154488
Shenal Dhanusekera <43013703+ShenalDhanusekera@users.noreply.github.com>
2020-01-22 12:34:40 -0600

    Final
    Final

--- Pizza Hotline - Final/javascript/javascript.js
+++ Pizza Hotline - Final/javascript/javascript.js
@@ -1,4 +1,5 @@
-﻿function myFunction() {
+﻿//This is the Javascript for the Hamburger Menu. When the Screen is a specific witdh the Hamburger Menu will pop up. If not it will stay the same
+function myFunction() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
         x.className += " responsive";
